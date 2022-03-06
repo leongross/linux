b24674fd857136fcc7770a0b1f66d44d1e01522a
leongross <leon.gross@rub.de>
2022-03-06 20:30:46 +0100

    sync

--- rust/kernel/bindings_helper.h
+++ rust/kernel/bindings_helper.h
@@ -24,6 +24,9 @@
 #include <linux/irqdomain.h>
 #include <linux/amba/bus.h>
 #include <linux/gpio/driver.h>
+#include </linux/device-mapper.h>
+#include <linux/blk_types.h>
+#include <linux/bio.h>
 
 // `bindgen` gets confused at certain things
 const gfp_t BINDINGS_GFP_KERNEL = GFP_KERNEL;
