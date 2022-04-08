14a9fc287b3d11d79d6e933bf04bfaa2c9287bea
leongross <leon.gross@rub.de>
2022-04-08 14:00:08 +0200

    saved state

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -34,6 +34,8 @@
 #[cfg(not(CONFIG_RUST))]
 compile_error!("Missing kernel configuration for conditional compilation");
 
+pub mod call_me;
+
 #[cfg(not(test))]
 #[cfg(not(testlib))]
 mod allocator;
@@ -67,6 +69,8 @@ pub mod task;
 pub mod linked_list;
 mod raw_list;
 pub mod rbtree;
+
+pub mod crypto;
 
 #[doc(hidden)]
 pub mod module_param;
